564654ab94b5806e9c7c49e93085fd5305cbcbb6
EstebanMJ97 <EstebanMJ97@github.com>
2018-07-30 11:10:40 -0600

    Push de Clases

---
+++ DATA/Receta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA
+{
+    public class Receta
+    {
+        public int IdReceta { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime FechaDespacho { get; set; }
+
+        public int IdPaciente { get; set; }
+
+        public bool? Actual { get; set; }
+
+    }
+
+}
